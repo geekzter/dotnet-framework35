4f72beb4115b998235d65d65481b29b5e2ef7a59
Eric van Wijk <eric@van-wijk.com>
2021-09-03 08:08:07 +0000

    wcf, win forms, wpf (#2)

---
+++ wpf-framework35/App.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+
+namespace wpf_framework35
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
